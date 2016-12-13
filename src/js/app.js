a45636a76da8c792718c39e333daeee064607d85
BaptisteVillain <bvillain92@gmail.com>
2016-12-13 19:55:14 +0100

    JS integration

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,19 @@
-var test_interface = {};
-test_interface.iron         = document.querySelector('.iron');
-test_interface.carbon       = document.querySelector('.carbon');
-test_interface.silicon      = document.querySelector('.silicon');
-test_interface.power        = document.querySelector('.power');
-test_interface.click_number = document.querySelector('.click-number');
+var interface = {};
+interface.iron         = document.querySelector('#info-iron');
+interface.carbon       = document.querySelector('#info-carbon');
+interface.silicon      = document.querySelector('#info-silicon');
+interface.power        = document.querySelector('.power');
+interface.click_number = document.querySelector('.click-number');
 
-test_interface.info = {};
-test_interface.info.iron    = document.querySelector('.info-iron');
-test_interface.info.carbon  = document.querySelector('.info-carbon');
-test_interface.info.silicon = document.querySelector('.info-silicon');
-test_interface.info.power   = document.querySelector('.info-power');
+interface.autoclicker  = {};
 
-test_interface.cursor       = document.querySelector('.cursor');
-
-test_interface.autoclicker  = {};
-
-test_interface.target = {};
-test_interface.target.iron     = document.querySelector('.iron-target');
-test_interface.target.iron_pos = test_interface.target.iron.getBoundingClientRect();
-test_interface.target.carbon   = document.querySelector('.carbon-target');
-test_interface.target.carbon_pos = test_interface.target.carbon.getBoundingClientRect();
-test_interface.target.silicon  = document.querySelector('.silicon-target');
-test_interface.target.silicon_pos = test_interface.target.silicon.getBoundingClientRect();
+interface.target = {};
+interface.target.iron     = document.querySelector('#target-iron');
+interface.target.iron_pos = interface.target.iron.getBoundingClientRect();
+interface.target.carbon   = document.querySelector('#target-carbon');
+interface.target.carbon_pos = interface.target.carbon.getBoundingClientRect();
+interface.target.silicon  = document.querySelector('#target-silicon');
+interface.target.silicon_pos = interface.target.silicon.getBoundingClientRect();
 
 var time = 0;
 
@@ -84,9 +76,9 @@ function buy(i){
 
 
 function resize(){
-  test_interface.target.iron_pos = test_interface.target.iron.getBoundingClientRect();
-  test_interface.target.carbon_pos = test_interface.target.carbon.getBoundingClientRect();
-  test_interface.target.silicon_pos = test_interface.target.silicon.getBoundingClientRect();
+  interface.target.iron_pos = interface.target.iron.getBoundingClientRect();
+  interface.target.carbon_pos = interface.target.carbon.getBoundingClientRect();
+  interface.target.silicon_pos = interface.target.silicon.getBoundingClientRect();
 }
 
 window.addEventListener('resize',function(){
@@ -136,11 +128,11 @@ window.addEventListener('click',function(e){
 
 
 function update_interface(){
-  test_interface.iron.innerHTML         = ressources.ore.iron;
-  test_interface.carbon.innerHTML       = ressources.ore.carbon;
-  test_interface.silicon.innerHTML      = ressources.ore.silicon;
-  test_interface.power.innerHTML        = ressources.power.value;
-  test_interface.click_number.innerHTML = clicker.click_number;
+  interface.iron.innerHTML         = ressources.ore.iron;
+  interface.carbon.innerHTML       = ressources.ore.carbon;
+  interface.silicon.innerHTML      = ressources.ore.silicon;
+  //interface.power.innerHTML        = ressources.power.value;
+  //interface.click_number.innerHTML = clicker.click_number;
 }
 
 
@@ -179,16 +171,16 @@ function addparticles(e,type){
   click_particles.items[index].type = type;
 
   if(type == 'iron'){
-    click_particles.items[index].vx = ((test_interface.target.iron_pos.left+test_interface.target.iron_pos.width/4)-click_particles.items[index].x)/80;
-    click_particles.items[index].vy = ((test_interface.target.iron_pos.top+test_interface.target.iron_pos.height/4+130) -click_particles.items[index].y)/80;
+    click_particles.items[index].vx = ((interface.target.iron_pos.left+interface.target.iron_pos.width/4)-click_particles.items[index].x)/80;
+    click_particles.items[index].vy = ((interface.target.iron_pos.top+interface.target.iron_pos.height/4+130) -click_particles.items[index].y)/80;
   }
   else if(type == 'carbon'){
-    click_particles.items[index].vx = ((test_interface.target.carbon_pos.left+test_interface.target.carbon_pos.width/4)-click_particles.items[index].x)/80;
-    click_particles.items[index].vy = ((test_interface.target.carbon_pos.top+test_interface.target.carbon_pos.height/4+130) -click_particles.items[index].y)/80;
+    click_particles.items[index].vx = ((interface.target.carbon_pos.left+interface.target.carbon_pos.width/4)-click_particles.items[index].x)/80;
+    click_particles.items[index].vy = ((interface.target.carbon_pos.top+interface.target.carbon_pos.height/4+130) -click_particles.items[index].y)/80;
   }
   else if(type == 'silicon'){
-    click_particles.items[index].vx = ((test_interface.target.silicon_pos.left+test_interface.target.silicon_pos.width/4)-click_particles.items[index].x)/80;
-    click_particles.items[index].vy = ((test_interface.target.silicon_pos.top+test_interface.target.silicon_pos.height/4+130) -click_particles.items[index].y)/80;
+    click_particles.items[index].vx = ((interface.target.silicon_pos.left+interface.target.silicon_pos.width/4)-click_particles.items[index].x)/80;
+    click_particles.items[index].vy = ((interface.target.silicon_pos.top+interface.target.silicon_pos.height/4+130) -click_particles.items[index].y)/80;
   }
 
   click_particles.items[index].gy = 15;
@@ -215,14 +207,14 @@ function updateParticles(){
       click_particles.items[i].gx = 0;
     }
 
-    if(click_particles.items[i].type == 'iron' && (click_particles.items[i].x > test_interface.target.iron_pos.left && click_particles.items[i].x < test_interface.target.iron_pos.left + test_interface.target.iron_pos.width && click_particles.items[i].y > test_interface.target.iron_pos.top && click_particles.items[i].y < test_interface.target.iron_pos.top + test_interface.target.iron_pos.height)){
+    if(click_particles.items[i].type == 'iron' && (click_particles.items[i].x > interface.target.iron_pos.left && click_particles.items[i].x < interface.target.iron_pos.left + interface.target.iron_pos.width && click_particles.items[i].y > interface.target.iron_pos.top && click_particles.items[i].y < interface.target.iron_pos.top + interface.target.iron_pos.height)){
       txtParticleAdd(click_particles.items[i].type);
       click_particles.items.splice(i,1);
       click_particles.settings.index--;
       ressources.ore.iron += clicker.click_increment_value;
       update_interface();
     }
-    else if(click_particles.items[i].type == 'carbon' && (click_particles.items[i].x > test_interface.target.carbon_pos.left && click_particles.items[i].x < test_interface.target.carbon_pos.left + test_interface.target.carbon_pos.width && click_particles.items[i].y > test_interface.target.carbon_pos.top && click_particles.items[i].y < test_interface.target.carbon_pos.top + test_interface.target.carbon_pos.height)){
+    else if(click_particles.items[i].type == 'carbon' && (click_particles.items[i].x > interface.target.carbon_pos.left && click_particles.items[i].x < interface.target.carbon_pos.left + interface.target.carbon_pos.width && click_particles.items[i].y > interface.target.carbon_pos.top && click_particles.items[i].y < interface.target.carbon_pos.top + interface.target.carbon_pos.height)){
       txtParticleAdd(click_particles.items[i].type);
       click_particles.items.splice(i,1);
       click_particles.settings.index--;
@@ -230,7 +222,7 @@ function updateParticles(){
       update_interface();
     }
 
-    else if(click_particles.items[i].type == 'silicon' && (click_particles.items[i].x > test_interface.target.silicon_pos.left && click_particles.items[i].x < test_interface.target.silicon_pos.left + test_interface.target.silicon_pos.width && click_particles.items[i].y > test_interface.target.silicon_pos.top && click_particles.items[i].y < test_interface.target.silicon_pos.top + test_interface.target.silicon_pos.height)){
+    else if(click_particles.items[i].type == 'silicon' && (click_particles.items[i].x > interface.target.silicon_pos.left && click_particles.items[i].x < interface.target.silicon_pos.left + interface.target.silicon_pos.width && click_particles.items[i].y > interface.target.silicon_pos.top && click_particles.items[i].y < interface.target.silicon_pos.top + interface.target.silicon_pos.height)){
       txtParticleAdd(click_particles.items[i].type);
       click_particles.items.splice(i,1);
       click_particles.settings.index--;
@@ -251,7 +243,7 @@ function drawParticles(i){
   img.style.position = "absolute";
   img.style.transform  = 'translate('+ click_particles.items[i].x +'px, '+ click_particles.items[i].y +'px)';
   img.src = "assets/img/ore-"+ click_particles.items[i].type +".svg";
-  document.querySelector('.cursors').appendChild(img);
+  document.querySelector('.particles-container .ressources').appendChild(img);
 }
 
 
@@ -265,16 +257,16 @@ function txtParticleAdd(type){
   var index = txtParticles.settings.index;
   txtParticles.items[index] = {};
   if(type == 'iron'){
-    txtParticles.items[index].x = test_interface.target.iron_pos.left-20;
-    txtParticles.items[index].y = test_interface.target.iron_pos.top + test_interface.target.iron_pos.width/2;
+    txtParticles.items[index].x = interface.target.iron_pos.left-20;
+    txtParticles.items[index].y = interface.target.iron_pos.top + interface.target.iron_pos.width/2;
   }
   else if(type == 'carbon'){
-    txtParticles.items[index].x = test_interface.target.carbon_pos.left-20;
-    txtParticles.items[index].y = test_interface.target.carbon_pos.top + test_interface.target.carbon_pos.width/2;
+    txtParticles.items[index].x = interface.target.carbon_pos.left-20;
+    txtParticles.items[index].y = interface.target.carbon_pos.top + interface.target.carbon_pos.width/2;
   }
   else if(type == 'silicon'){
-    txtParticles.items[index].x = test_interface.target.silicon_pos.left-20;
-    txtParticles.items[index].y = test_interface.target.silicon_pos.top + test_interface.target.silicon_pos.width/2;
+    txtParticles.items[index].x = interface.target.silicon_pos.left-20;
+    txtParticles.items[index].y = interface.target.silicon_pos.top + interface.target.silicon_pos.width/2;
   }
   txtParticles.items[index].vx = -1;
   txtParticles.items[index].vy = -1;
@@ -305,18 +297,18 @@ function txtParticleUpdate(){
 
 function txtParticlesDraw(i){
   var txt = document.createElement('p');
-  txt.className = "indication";
   txt.style.transform = 'translate('+ txtParticles.items[i].x+'px,'+ txtParticles.items[i].y+'px)';
+  txt.style.width ='10px';
   txt.style.opacity =  txtParticles.items[i].opacity;
   txt.innerHTML = "+" + clicker.click_increment_value;
-  document.querySelector('.txtcursors').appendChild(txt);
+  document.querySelector('.particles-container .indication').appendChild(txt);
 }
 
 
 
 function render(){
-  document.querySelector('.cursors').innerHTML = "";
-  document.querySelector('.txtcursors').innerHTML = "";
+  document.querySelector('.particles-container .ressources').innerHTML = "";
+  document.querySelector('.particles-container .indication').innerHTML = "";
   updateParticles();
   txtParticleUpdate();
   window.requestAnimationFrame(render);
